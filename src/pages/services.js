24ffdfe3c407ed3724188c3f01e6533782caf0ba
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2020-06-23 19:50:32 +0200

    add static content to the service cards

--- src/pages/services.js
+++ src/pages/services.js
@@ -25,7 +25,7 @@ const Article = styled.article`
 
 const ArticleCards = styled.div`
   display: grid;
-  grid-gap: 20px;
+  grid-gap: 40px;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(2, 1fr);
   margin-top: 20px;
@@ -34,12 +34,23 @@ const ArticleCards = styled.div`
 const ArticleCard = styled.article`
   border: solid 1px ${({ theme }) => theme.colors.grey100};
   width: 100%;
-  height: 534px;
+  min-height: 600px;
   position: relative;
   transition: background-color 0.5s ease-in-out;
 
   &:hover {
     background-color: ${({ theme }) => theme.colors.grey100};
+  }
+
+  ul {
+    list-style: none;
+    color: ${({ theme }) => theme.colors.primary};
+    font-family: ${({ theme }) => theme.fontFamily.open};
+    font-weight: ${({ theme }) => theme.fontWeight.medium};
+
+    li {
+      background-image: url("../images/tick-icon.svg");
+    }
   }
 `
 
@@ -73,28 +84,84 @@ const ServicesPage = ({ data }) => (
       <WelcomeHeader type="gallery" />
       <ArticleCards>
         <ArticleCard>
+          <CrossDecoration type="left-top" />
+          <CrossDecoration type="right-top" />
+          <CrossDecoration type="left-bottom" />
+          <CrossDecoration type="right-bottom" />
           <ArticleImage image={data.file.publicURL} alt="hero"></ArticleImage>
           <ArticleHeader>glazura i terakota</ArticleHeader>
+          <ul>
+            <li>szlifowanie narozników</li>
+            <li>układanie glazury i terakoty</li>
+            <li>układanie gresu</li>
+          </ul>
         </ArticleCard>
         <ArticleCard>
+          <CrossDecoration type="left-top" />
+          <CrossDecoration type="right-top" />
+          <CrossDecoration type="left-bottom" />
+          <CrossDecoration type="right-bottom" />
           <ArticleImage image={data.file.publicURL} alt="hero"></ArticleImage>
-          <ArticleHeader>glazura i terakota</ArticleHeader>
+          <ArticleHeader>prace wykończeniowe</ArticleHeader>
+          <ul>
+            <li>układanie paneli podłogowych</li>
+            <li>malowanie</li>
+            <li>montaż armatury wykończeniowej</li>
+            <li>zabudowa karton-gips</li>
+            <li>zabezpieczenie miejsc pracy</li>
+          </ul>
         </ArticleCard>
         <ArticleCard>
+          <CrossDecoration type="left-top" />
+          <CrossDecoration type="right-top" />
+          <CrossDecoration type="left-bottom" />
+          <CrossDecoration type="right-bottom" />
           <ArticleImage image={data.file.publicURL} alt="hero"></ArticleImage>
-          <ArticleHeader>glazura i terakota</ArticleHeader>
+          <ArticleHeader>armatura łazienkowa</ArticleHeader>
+          <ul>
+            <li>montaż armatury łazienkowej</li>
+            <li>dobór materiałów</li>
+            <li>profesjonalne doradztwo</li>
+          </ul>
         </ArticleCard>
         <ArticleCard>
+          <CrossDecoration type="left-top" />
+          <CrossDecoration type="right-top" />
+          <CrossDecoration type="left-bottom" />
+          <CrossDecoration type="right-bottom" />
           <ArticleImage image={data.file.publicURL} alt="hero"></ArticleImage>
-          <ArticleHeader>glazura i terakota</ArticleHeader>
+          <ArticleHeader>aranżacja projektu</ArticleHeader>
+          <ul>
+            <li>prace pod projekt</li>
+            <li>współpraca z biurem projektowym</li>
+            <li>profesjonalne doradztwo w doborze materiałów</li>
+          </ul>
         </ArticleCard>
         <ArticleCard>
+          <CrossDecoration type="left-top" />
+          <CrossDecoration type="right-top" />
+          <CrossDecoration type="left-bottom" />
+          <CrossDecoration type="right-bottom" />
           <ArticleImage image={data.file.publicURL} alt="hero"></ArticleImage>
-          <ArticleHeader>glazura i terakota</ArticleHeader>
+          <ArticleHeader>elektryka i oświetlenie</ArticleHeader>
+          <ul>
+            <li>montaż skrzyń bezpiecznikowych</li>
+            <li>montaż oświetlenia</li>
+            <li>profesjonalne doradztwo w doborze materiałów</li>
+          </ul>
         </ArticleCard>
         <ArticleCard>
+          <CrossDecoration type="left-top" />
+          <CrossDecoration type="right-top" />
+          <CrossDecoration type="left-bottom" />
+          <CrossDecoration type="right-bottom" />
           <ArticleImage image={data.file.publicURL} alt="hero"></ArticleImage>
-          <ArticleHeader>glazura i terakota</ArticleHeader>
+          <ArticleHeader>prace hydrauliczne</ArticleHeader>
+          <ul>
+            <li>podłączanie rur wodno-kanalizacyjnych</li>
+            <li>wykuwanie bruzd i punktów</li>
+            <li>inne prace hydrauliczne</li>
+          </ul>
         </ArticleCard>
       </ArticleCards>
     </Article>
