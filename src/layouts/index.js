69a5ad5b58e29408096a6b7386afbb955d565376
Krzysztof Grudzień <krzysztof.grudzien.fed@gmail.com>
2020-06-29 11:38:50 +0200

    feat(layout): add additional box wrapper covering main structure and setup grid

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,15 +3,22 @@ import Header from "../components/header"
 import Footer from "../components/footer"
 import GlobalStyle from "../styles/globalStyles"
 import { theme } from "../styles/themeStyles"
-import { ThemeProvider } from "styled-components"
+import styled, { ThemeProvider } from "styled-components"
 
+const Container = styled.div`
+  display: grid;
+  grid-template-rows: 103px 1fr 103px;
+  grid-template-columns: 1fr;
+`
 const Layout = ({ children }) => (
   <>
     <GlobalStyle />
     <ThemeProvider theme={theme}>
-      <Header />
-      {children}
-      <Footer />
+      <Container>
+        <Header />
+        {children}
+        <Footer />
+      </Container>
     </ThemeProvider>
   </>
 )
